dac574975bc6e578a19853134a43e97d0541773d
hubinfourniret <hubin.fourniret@sts-sio-caen.info>
2023-12-24 18:12:18 +0100

    123

--- php/A_propos.php
+++ php/A_propos.php
@@ -4,19 +4,14 @@ use Symfony\Component\Yaml\Yaml;
 // Chargement du fichier YAML
 $data = Yaml::parseFile('./../YAML/A_propos.yaml');
 
-// Accès aux données
-$title = $data['title'];
-$bienvenue = $data['bien'];
-$accroche = $data['accroche'];
-
 // Affichage des données
-echo "<h1>" . $title . "</h1>";
+echo "<h1>" . $data['title'] . "</h1>";
 
 // Affichage de l'accroche
 echo "<div class='para'>";
-echo "<br>" . $bienvenue . "</br>";
-foreach ($accroche as $paragraphe) {
-    echo "<p>" . $paragraphe . "</p>";
+echo "<br>" . $data['debut-accroche'] . "</br>";
+foreach ($accroche as $data['paragraphe']) {
+    echo "<p>" . $data['paragraphe'] . "</p>";
 }
 echo "</div>";
 ?>
